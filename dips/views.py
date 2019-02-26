d2c27d4fb4e2cfb961afe9223588c438696cb4c1
José Raddaoui Marín <raddaouimarin@gmail.com>
2019-02-26 20:10:10 +0100

    Change fields queried in digital file searches
    - Remove DIP identifier on DIP page.
    - Add Collection title in search page.

--- dips/views.py
+++ dips/views.py
@@ -272,7 +272,7 @@ def search(request):
             'terms',
             **{'dip.import_status': [DIP.IMPORT_PENDING, DIP.IMPORT_FAILURE]},
         )
-    fields = ['filepath', 'fileformat', 'dip.identifier']
+    fields = ['filepath', 'fileformat', 'dip.identifier', 'collection.title']
     search = add_query_to_search(search, request, fields)
     search = search.sort({sort_field: {'order': sort_dir}})
 
@@ -377,7 +377,7 @@ def dip(request, pk):
         'match',
         **{'dip.id': pk},
     )
-    fields = ['filepath', 'fileformat', 'dip.identifier']
+    fields = ['filepath', 'fileformat']
     search = add_query_to_search(search, request, fields)
     search = search.sort({sort_field: {'order': sort_dir}})
 
